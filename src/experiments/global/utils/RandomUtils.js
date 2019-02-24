087ae7dd9e02f287e049a7d3a7193129ac66fe45
Yann Uzel <yann.uzel@gmail.com>
2019-02-24 22:27:18 +0100

    Updates

--- src/experiments/global/utils/RandomUtils.js
+++
@@ -1,7 +0,0 @@
-const randomUtils = {};
-
-randomUtils.rndInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
-
-randomUtils.rndElem = (array) => array[randomUtils.rndInt(0, array.length)];
-
-export default randomUtils;
